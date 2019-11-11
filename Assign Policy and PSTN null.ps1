0548d23ec3f3bc9fdb89e6e512634c055a19a1cc
manic2003 <54588465+manic2003@users.noreply.github.com>
2019-11-11 10:41:46 +0000

    Add files via upload

---
+++ Assign Policy and PSTN null.ps1
@@ -0,0 +1,10 @@
+﻿$user = "logisticscallgroup@peakscientific.com"
+
+Grant-CsOnlineVoiceRoutingPolicy -Identity $user -PolicyName "UK Calls"
+
+Set-CsUserPstnSettings -Identity $user -AllowInternationalCalls $true -HybridPSTNSite $null
+
+Get-CsOnlineUser -Identity $user | fl *ONLINEVOICEROUTING*
+
+Get-CsUserPstnSettings -Identity $user
+Get-CsOnlineUser -Identity $user | select onpremlineuri
