b5e6c73b9a0b83f5d431d20765397d095c40fe7e
manic2003 <54588465+manic2003@users.noreply.github.com>
2019-11-11 10:41:24 +0000

    Add files via upload

---
+++ CheckDialPlans.ps1
@@ -0,0 +1,9 @@
+﻿Test-CsEffectiveTenantDialPlan -DialedNumber 8007212508 -Identity cmurray@peakscientific.com
+
+Get-CsEffectiveTenantDialPlan -Identity smahnke@peakscientific.com | Test-CsEffectiveTenantDialPlan -DialedNumber 8007212508
+
+Get-CsEffectiveTenantDialPlan -Identity emuellers@peakscientific.com
+
+Get-CsEffectiveTenantDialPlan -Identity emuellers@peakscientific.com | Test-CsEffectiveTenantDialPlan -DialedNumber 016091565900
+
+Get-CsTenantDialPlan  -Identity GermanyDP
